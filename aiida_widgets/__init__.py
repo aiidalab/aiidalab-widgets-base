759666cf457f616f29de752c0d1630af4e2ecfd5
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-10-11 14:48:35 +0200

    prepare v0.2.0a1 release

--- aiida_widgets/__init__.py
+++ aiida_widgets/__init__.py
@@ -3,4 +3,4 @@ from .structures import StructureUploadWidget  # noqa
 from .structures_multi import MultiStructureUploadWidget  # noqa
 from .codes import CodeDropdown  # noqa
 
-__version__ = "0.2.0a0"
+__version__ = "0.2.0a1"
