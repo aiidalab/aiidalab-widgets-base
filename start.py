5aab8d8bfda9cc414c9591d94be313f315df2b84
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-10-12 01:05:22 +0200

    Add reference to structures_muli.ipynb in start.py

--- start.py
+++ start.py
@@ -6,7 +6,8 @@ template = """
   <th style="text-align:center">AiiDA Lab widgets</th>
 <tr>
   <td valign="top"><ul>
-    <li><a href="{appbase}/structures.ipynb" target="_blank">Dealing with structures</a></li>
+    <li><a href="{appbase}/structures.ipynb" target="_blank">Dealing with one structure</a></li>
+    <li><a href="{appbase}/structures_multi.ipynb" target="_blank">Dealing with multiple structures</a></li>
     <li><a href="{appbase}/example.ipynb" target="_blank">Dealing with codes</a></li>
   </ul></td>
 </tr>
