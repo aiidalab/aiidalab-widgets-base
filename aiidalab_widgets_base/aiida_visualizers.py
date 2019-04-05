5376ae926428e25c6be73ab88c085bea7ac0273a
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2019-04-05 15:00:00 +0000

    Add an option allowing to export and download aiida objects

--- aiidalab_widgets_base/aiida_visualizers.py
+++ aiidalab_widgets_base/aiida_visualizers.py
@@ -9,40 +9,87 @@ if not is_dbenv_loaded():
 
 class ParameterDataVisualizer(ipw.HTML):
     """Basic visualizer class for ParameterData object"""
-    def __init__(self, parameter, **kwargs):
+    def __init__(self, parameter, downloadable=True, **kwargs):
         import pandas as pd
         import base64
         from IPython.display import FileLink
         df = pd.DataFrame([(key, value) for key, value in parameter.get_dict().items()], columns=['key', 'value'])
         self.value = df.to_html(index=False)
-        payload = base64.b64encode(df.to_csv().encode()).decode()
-        fname = '{}.csv'.format(parameter.pk)
-        self.value += """<a download="{filename}" href="data:text/csv;base64,{payload}" target="_blank">{title}</a>"""
-        self.value = self.value.format(filename=fname, payload=payload,title=fname)
+        if downloadable:
+            payload = base64.b64encode(df.to_csv().encode()).decode()
+            fname = '{}.csv'.format(parameter.pk)
+            to_add = """<a download="{filename}" href="data:text/csv;base64,{payload}" target="_blank">{title}</a>"""
+            self.value += to_add.format(filename=fname, payload=payload,title=fname)
         super(ParameterDataVisualizer, self).__init__(**kwargs)
 
 class StructureDataVisualizer(ipw.VBox):
     """Basic visualizer class for StructureData object"""
-    def __init__(self, structure, **kwargs):
+    def __init__(self, structure, downloadable=True, **kwargs):
         import nglview
+        self._structure = structure
         viewer = nglview.NGLWidget()
-        viewer.add_component(nglview.ASEStructure(structure.get_ase()))  # adds ball+stick
+        viewer.add_component(nglview.ASEStructure(self._structure.get_ase()))  # adds ball+stick
         viewer.add_unitcell()
         children = [viewer]
+        if downloadable:
+            self.file_format = ipw.Dropdown(
+                options=['xyz', 'cif', 'json'],
+                description="File format:",
+            )
+            self.download_btn = ipw.Button(description="Download")
+            self.download_btn.on_click(self.download)
+            children.append(ipw.HBox([self.file_format, self.download_btn]))
         super(StructureDataVisualizer, self).__init__(children, **kwargs)
 
-class FolderDataVisualizer(ipw.HBox):
+    def download(self, b=None):
+        import base64
+        from tempfile import TemporaryFile
+        from IPython.display import Javascript
+        with TemporaryFile() as fobj:
+            self._structure.get_ase().write(fobj, format=self.file_format.value)
+            fobj.seek(0)
+            b64 = base64.b64encode(fobj.read())
+            payload = b64.decode()
+        js = Javascript(
+            """
+            var link = document.createElement('a');
+            link.href = "data:;base64,{payload}"
+            link.download = "{filename}"
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            """.format(payload=payload,filename=str(self._structure.id)+'.'+self.file_format.value)
+        )
+        display(js)
+
+class FolderDataVisualizer(ipw.VBox):
     """Basic visualizer class for FolderData object"""
-    def __init__(self, folder, **kwargs):
+    def __init__(self, folder, downloadable=True, **kwargs):
+        import random
+        import string
         self._folder = folder
         self.files = ipw.Dropdown(
             options=self._folder.get_folder_list(),
             description="Select file:",
         )
-        self.download_btn = ipw.Button(description="Download")
-        self.download_btn.on_click(self.download)
-        children = [self.files, self.download_btn]
+        self.text = ipw.Textarea(
+            value="",
+            description='File content:',
+            layout={'width':"900px", 'height':'300px'},
+            disabled=False
+        )
+        self.change_file_view()
+        self.files.observe(self.change_file_view, names='value')
+        children = [self.files, self.text]
+        if downloadable:
+            self.download_btn = ipw.Button(description="Download")
+            self.download_btn.on_click(self.download)
+            children.append(self.download_btn)
         super(FolderDataVisualizer, self).__init__(children, **kwargs)
+
+    def change_file_view(self, b=None):
+        with open(self._folder.get_abs_path(self.files.value), "rb") as fobj:
+            self.text.value = fobj.read()
 
     def download(self, b=None):
         import base64
--- aiidalab_widgets_base/display.py
+++ aiidalab_widgets_base/display.py
@@ -8,12 +8,12 @@ RECOGNIZED_AIIDA_DATA_VISUALIZERS = {'data.parameter.ParameterData.' : 'Paramete
                                      'data.cif.CifData.' : 'StructureDataVisualizer',
                                      'data.folder.FolderData.' : 'FolderDataVisualizer',}
 
-def aiidalab_display(obj):
+def aiidalab_display(obj, downloadable=True):
     """Function that is able to display properly differnt AiiDA data types"""
     from aiidalab_widgets_base import aiida_visualizers
     if hasattr(obj, 'type') and obj.type in RECOGNIZED_AIIDA_DATA_VISUALIZERS:
         aiidavis_type = RECOGNIZED_AIIDA_DATA_VISUALIZERS[obj.type]
         imported = getattr(aiida_visualizers, aiidavis_type)
-        display(imported(obj))
+        display(imported(obj, downloadable=downloadable))
     else:
         display(obj)
