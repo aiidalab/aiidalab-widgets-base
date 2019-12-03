dcb97879fbcba6859376a7c29e172b4f780b7faf
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2019-12-03 20:56:19 +0100

    Work on AiiDA objects viewers (#28)
    * Replace `aiidalab_display` function with a `viewer` function that returns a
    a viewer object able to display the given AiiDA object:
    * StructureVisualizer:
      1) Add thumbnail property
      2) Simplify and unify the conversion to cif, xyz and png formats

--- aiidalab_widgets_base/__init__.py
+++ aiidalab_widgets_base/__init__.py
@@ -4,11 +4,12 @@ from __future__ import absolute_import
 from aiida import load_profile
 load_profile()
 
+from .aiida_viewers import viewer  # noqa
 from .codes import CodeDropdown, AiiDACodeSetup, valid_aiidacode_args  # noqa
 from .computers import SshComputerSetup, valid_sshcomputer_args  # noqa
 from .computers import AiidaComputerSetup, valid_aiidacomputer_args  # noqa
+from .computers import ComputerDropdown  # noqa
 from .databases import CodQueryWidget  # noqa
-from .display import aiidalab_display  # noqa
 from .export import ExportButton  # noqa
 from .metadata import MetadataWidget  # noqa
 from .process import ProgressBar, RunningCalcJobOutput  # noqa
