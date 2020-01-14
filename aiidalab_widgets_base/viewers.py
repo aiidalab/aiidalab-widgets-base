25f9c88aa9bb7649a31ae54a5dd8b99092ec06a5
Carl Simon Adorf <csadorf@umich.edu>
2020-01-14 11:46:28 +0100

    Remove six module imports.

--- aiidalab_widgets_base/viewers.py
+++ aiidalab_widgets_base/viewers.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 import ipywidgets as ipw
-from six.moves import range
 from IPython.display import display
 import nglview
 
