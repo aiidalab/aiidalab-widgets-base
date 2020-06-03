4122f1fa97d69faddfd36c60064b25d17c318629
Carlo Pignedoli <c.pignedoli@gmail.com>
2020-06-03 19:13:45 +0000

    added button to move selection directly to xyz position

--- aiidalab_widgets_base/structures.py
+++ aiidalab_widgets_base/structures.py
@@ -560,6 +560,8 @@ class BasicStructureEditor(ipw.VBox):
 
         btn_move_dxyz = ipw.Button(description='Move', layout={'width': 'initial'})
         btn_move_dxyz.on_click(self.translate_dxdydz)
+        btn_move_to_xyz = ipw.Button(description='Move to', layout={'width': 'initial'})
+        btn_move_to_xyz.on_click(self.translate_to_xyz)
         self.dxyz = ipw.Text(description='Move along (XYZ)',
                              value='0 0 0',
                              style={'description_width': 'initial'},
@@ -625,7 +627,8 @@ class BasicStructureEditor(ipw.VBox):
                      layout={'margin': '0px 0px 0px 20px'}),
             ipw.HBox([self.point, btn_def_pnt, self.autoclear_selection], layout={'margin': '0px 0px 0px 20px'}),
             ipw.HTML("<b>Move atom(s):</b>", layout={'margin': '20px 0px 10px 0px'}),
-            ipw.HBox([self.displacement, btn_move_dr, self.dxyz, btn_move_dxyz], layout={'margin': '0px 0px 0px 20px'}),
+            ipw.HBox([self.displacement, btn_move_dr, self.dxyz, btn_move_dxyz, btn_move_to_xyz],
+                     layout={'margin': '0px 0px 0px 20px'}),
             ipw.HBox([self.phi, btn_rotate], layout={'margin': '0px 0px 0px 20px'}),
             ipw.HTML("<b>Modify atom(s):</v>", layout={'margin': '20px 0px 10px 0px'}),
             ipw.HBox([self.element, self.ligand], layout={'margin': '0px 0px 0px 20px'}),
@@ -708,6 +711,19 @@ class BasicStructureEditor(ipw.VBox):
 
         # The action.
         atoms.positions[self.selection] += np.array(self.str2vec(self.dxyz.value))
+
+        self.structure = atoms
+        self.selection = selection
+
+    def translate_to_xyz(self, _=None):
+        """Translate along the selected vector."""
+        selection = self.selection
+        atoms = self.structure.copy()
+
+        # The action.
+        geo_center = np.average(self.structure[self.selection].get_positions(), axis=0)
+
+        atoms.positions[self.selection] += np.array(self.str2vec(self.dxyz.value) - geo_center)
 
         self.structure = atoms
         self.selection = selection
