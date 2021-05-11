2dc5116d5a5d17298858306c1950c569b9a236be
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2021-05-11 18:14:25 +0200

    Switch setup.json to setup.cfg, use vapory instead of aiidalab-vapory (#202)

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
-# -*- coding: utf-8 -*-
-"""Setting up CP2K plugin for AiiDA"""
-import json
+# -*- coding: utf8 -*-
+"""This file is required for editable installs of the package."""
+from setuptools import setup
 
-from io import open  # pylint: disable=redefined-builtin
-from setuptools import setup, find_packages
-
-
-def run_setup():
-    with open("setup.json", "r", encoding="utf-8") as info:
-        kwargs = json.load(info)
-    setup(
-        include_package_data=True,
-        packages=find_packages(),
-        long_description=open("README.md", encoding="utf-8").read(),
-        long_description_content_type="text/markdown",
-        **kwargs
-    )
-
-
-if __name__ == "__main__":
-    run_setup()
+setup()
