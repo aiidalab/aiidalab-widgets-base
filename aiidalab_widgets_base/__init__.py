f569aa404207807c1d0cf6b4dc095423f476d1a9
Leopold Talirz <leopold.talirz@gmail.com>
2021-09-15 14:07:51 +0200

    remove multistructureuploadwidget
    was apparently forgotten to remove

--- aiidalab_widgets_base/__init__.py
+++ aiidalab_widgets_base/__init__.py
@@ -50,7 +50,6 @@ __all__ = [
     "ElnExportWidget",
     "ElnImportWidget",
     "ExportButtonWidget",
-    "MultiStructureUploadWidget",
     "NodesTreeWidget",
     "OpenAiidaNodeInAppWidget",
     "OptimadeQueryWidget",
