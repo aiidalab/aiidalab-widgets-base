65ba6bc80c001c0bdb3ba9c64d8c893e40f935ba
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2022-01-25 17:13:15 +0100

    Add a class to handle Pinhole camera.
    This allows to easily handle transformations between 3D "real" space
    and the "screen" space.

--- aiidalab_widgets_base/utils/__init__.py
+++ aiidalab_widgets_base/utils/__init__.py
@@ -2,6 +2,7 @@
 from functools import wraps
 
 import more_itertools as mit
+import numpy as np
 from ase.io import read
 
 
@@ -115,3 +116,20 @@ def yield_for_change(widget, attribute):
         return inner
 
     return f
+
+
+class PinholeCamera:
+    def __init__(self, matrix):
+        self.matrix = np.reshape(matrix, (4, 4)).transpose()
+
+    def screen_to_vector(self, move_vector):
+        """Converts vector from the screen coordinates to the normalized vector in 3D."""
+        move_vector[0] = -move_vector[0]  # the x axis seem to be reverted in nglview.
+        res = np.append(np.array(move_vector), [0])
+        res = self.inverse_matrix.dot(res)
+        res /= np.linalg.norm(res)
+        return res[0:3]
+
+    @property
+    def inverse_matrix(self):
+        return np.linalg.inv(self.matrix)
