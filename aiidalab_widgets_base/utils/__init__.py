b00d3fd9cb55b6d83afac35c50e47d74e1907e7e
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2022-08-15 19:40:46 +0100

    StructureBrowserWidget: Customizable query types

--- aiidalab_widgets_base/utils/__init__.py
+++ aiidalab_widgets_base/utils/__init__.py
@@ -5,7 +5,12 @@ import ipywidgets as ipw
 import more_itertools as mit
 import numpy as np
 import traitlets
+from aiida.plugins import DataFactory
 from ase.io import read
+
+CifData = DataFactory("cif")  # pylint: disable=invalid-name
+StructureData = DataFactory("structure")  # pylint: disable=invalid-name
+TrajectoryData = DataFactory("array.trajectory")  # pylint: disable=invalid-name
 
 
 def valid_arguments(arguments, valid_args):
@@ -79,6 +84,21 @@ def string_range_to_list(strng, shift=-1):
         except ValueError:
             return list(), False
     return singles, True
+
+
+def get_formula(data_node):
+    """A wrapper for getting a molecular formula out of the AiiDA Data node"""
+    if isinstance(data_node, TrajectoryData):
+        # TrajectoryData can only hold structures with the same chemical formula,
+        # so this approach is sound.
+        stepid = data_node.get_stepids()[0]
+        return data_node.get_step_structure(stepid).get_formula()
+    elif isinstance(data_node, StructureData):
+        return data_node.get_formula()
+    elif isinstance(data_node, CifData):
+        return data_node.get_ase().get_chemical_formula()
+    else:
+        raise ValueError(f"Cannot get formula from node {type(data_node)}")
 
 
 class PinholeCamera:
