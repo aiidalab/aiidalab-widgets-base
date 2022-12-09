3aece7f009afc6eb77829f0fafb5802b279d5661
Daniel Hollas <danekhollas@gmail.com>
2022-12-09 16:21:19 +0000

    Update comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,9 +33,10 @@ def notebook_service(docker_ip, docker_services):
 
     docker_compose = docker_services._docker_compose
 
-    # assurance for host user UID other that 1000
-    chown_command = "exec -T -u root aiidalab bash -c 'chmod -R a+rw /home/jovyan/apps/aiidalab-widgets-base'"
-    docker_compose.execute(chown_command)
+    # Directory ~/apps/aiidalab-widgets-base/ is mounted by docker,
+    # make it writeable for jovyan user, needed for `pip install`
+    chmod_command = "exec -T -u root aiidalab bash -c 'chmod -R a+rw /home/jovyan/apps/aiidalab-widgets-base'"
+    docker_compose.execute(chmod_command)
 
     install_command = "bash -c 'pip install .'"
     command = f"exec --workdir /home/jovyan/apps/aiidalab-widgets-base -T aiidalab {install_command}"
@@ -60,7 +61,10 @@ def selenium_driver(selenium, notebook_service):
             url, f"apps/apps/aiidalab-widgets-base/{nb_path}?token={token}"
         )
         selenium.get(f"{url_with_token}")
-        selenium.implicitly_wait(10)  # must wait until the app loaded
+        # By default, let's allow selenium functions to retry for 10s
+        # till a given element is loaded, see:
+        # https://selenium-python.readthedocs.io/waits.html#implicit-waits
+        selenium.implicitly_wait(10)
         window_height = 800
         window_width = 900
         selenium.set_window_size(window_width, window_height)
