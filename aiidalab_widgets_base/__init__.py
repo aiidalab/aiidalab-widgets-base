92e220e87b993aa081efeb54d1a7cd0599b03cc0
Jusong Yu <jusong.yeu@gmail.com>
2023-07-27 16:10:20 +0000

    Backward compatible with throwing a warning

--- aiidalab_widgets_base/__init__.py
+++ aiidalab_widgets_base/__init__.py
@@ -1,4 +1,31 @@
 """Reusable widgets for AiiDAlab applications."""
+from aiida.manage import get_profile
+
+_WARNING_TEMPLATE = """
+<div style="background-color: #f7f7f7; border: 2px solid #e0e0e0; padding: 20px; border-radius: 5px;">
+    <p style="font-size: 16px; font-weight: bold; color: #ff5733;">Warning:</p>
+    <p style="font-size: 14px;">The default profile '<span style="font-style: italic;">{profile}</span>' was loaded automatically. This behavior will be removed in the <span style="font-style: italic;">{version}</span>. Please load the profile manually before loading modules from aiidalab-widgets-base by adding the following code at the beginning cell of the notebook:</p>
+    <pre style="background-color: #f0f0f0; padding: 10px; border: 1px solid #ccc; font-family: 'Courier New', monospace;">
+from aiida import load_profile
+load_profile();</pre>
+</div>
+"""
+
+
+if get_profile() is None:
+    # if no profile is loaded, load the default profile and raise a deprecation warning
+    from aiida import load_profile
+    from IPython.display import display, HTML
+    
+    load_profile()
+    
+    profile = get_profile()
+    assert profile is not None, "Failed to load the default profile"
+    
+    # raise a deprecation warning
+    warning = HTML(_WARNING_TEMPLATE.format(profile=profile.name, version="v3.0.0"))
+    display(warning)
+
 from .computational_resources import (
     ComputationalResourcesWidget,
     ComputerDropdownWidget,
