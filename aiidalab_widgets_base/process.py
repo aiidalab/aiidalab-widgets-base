07359c59a0b62c03c6ef487230544476e2f189bf
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2023-09-07 17:51:41 +0000

    Fix ProcessFollowerWidget
    This was missed when we changed the ProcessMonitor
    to use Process UUID as its traitlet.

--- aiidalab_widgets_base/process.py
+++ aiidalab_widgets_base/process.py
@@ -302,7 +302,7 @@ class ProcessFollowerWidget(ipw.VBox):
 
         if self._monitor is None:
             self._monitor = ProcessMonitor(
-                process=self.process,
+                value=self.process.uuid,
                 callbacks=[self.update],
                 on_sealed=self._run_after_completed,
                 timeout=self.update_interval,
