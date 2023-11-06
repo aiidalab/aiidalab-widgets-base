a1368c3bfd960c13bd4681df7e51d688b2f0efe8
superstar54 <xingwang1991@gmail.com>
2023-11-06 15:50:20 +0000

    update test

--- tests/test_structures.py
+++ tests/test_structures.py
@@ -239,15 +239,15 @@ def test_basic_cell_editor_widget(structure_data_object):
 
     # Change the cell parameters.
     widget.cell_parameters.children[2].children[1].value = 10.0
-    widget.apply_cell_parameters()
+    widget._apply_cell_parameters()
     assert widget.structure.cell.cellpar()[2] == 10.0
 
     # make supercell using cell transformation
     widget.cell_transformation.children[0].children[0].value = 2
-    widget.apply_cell_transformation()
+    widget._apply_cell_transformation()
     assert widget.structure.get_chemical_formula() == "Si4"
     # reset the cell transformation matrix
-    widget.reset_cell_transformation_matrix()
+    widget._reset_cell_transformation_matrix()
     assert widget.cell_transformation.children[0].children[0].value == 1
 
 
