f5ca2f16661bf296e0e82cb2caf31051cf5f2cf1
Jusong Yu <jusong.yeu@gmail.com>
2024-02-15 22:49:57 +0000

    format HTML code tag

--- aiidalab_widgets_base/databases.py
+++ aiidalab_widgets_base/databases.py
@@ -161,8 +161,12 @@ class OptimadeQueryWidget(ipw.VBox):
         except ImportError:
             super().__init__(
                 [
-                    ipw.HTML(
-                        "This widget requires the `optimade-client` package to be installed."
+                    ipw.HTML("""
+                        <div style="margin: 10px 0; padding: 10px; background-color: #ffcc00; color: black; border-left: 6px solid #ffeb3b;">
+                            <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                            &nbsp;This widget requires the <code>optimade-client</code> package to be installed. 
+                            Please run <code>pip install optimade-client</code> to install the missing package.
+                        </div>"""
                     )
                 ]
             )
