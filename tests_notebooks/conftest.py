c4375a7dbe98ba54692be27e5d6fe130f7fdc907
Jusong Yu <jusong.yeu@gmail.com>
2024-02-21 13:29:20 +0000

    Use image from docker.io registry

--- tests_notebooks/conftest.py
+++ tests_notebooks/conftest.py
@@ -77,7 +77,9 @@ def notebook_service(docker_ip, docker_services, aiidalab_exec):
     output = aiidalab_exec("verdi --version").decode("utf-8").strip()
     after_version = output.split(" ")[-1]
 
-    assert before_version != after_version
+    assert (
+        before_version == after_version
+    ), f"aiida-core version was changed from {before_version} to {after_version}."
 
     # `port_for` takes a container port and returns the corresponding host port
     port = docker_services.port_for("aiidalab", 8888)
