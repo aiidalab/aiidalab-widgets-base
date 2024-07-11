fd643e709dc02d93624c88251b0eb2a158b5cfb5
Edan Bainglass <edan.bainglass@gmail.com>
2024-07-11 09:54:13 +0000

    Add css loader utility

---
+++ aiidalab_widgets_base/utils/loaders.py
@@ -0,0 +1,38 @@
+from importlib.resources import Package, files
+
+from IPython.display import Javascript, display
+
+
+def load_css_stylesheet(package: Package, filename: str = ""):
+    """Load a CSS stylesheet from a package and inject it into the DOM.
+
+    Parameters
+    ----------
+    `package` : `Package`
+        The package where the CSS file is located.
+    `filename` : `str`, optional
+        The name of the CSS file to load.
+        If not provided, all CSS files in the package will be loaded.
+    """
+    root = files(package)
+
+    filenames = []
+    if filename:
+        filenames.append(filename)
+    else:
+        filenames.extend(
+            path.name
+            for path in root.iterdir()
+            if path.is_file() and path.name.endswith(".css")
+        )
+
+    for filename in filenames:
+        stylesheet = (root / filename).read_text()
+        display(
+            Javascript(f"""
+                var style = document.createElement('style');
+                style.type = 'text/css';
+                style.innerHTML = `{stylesheet}`;
+                document.head.appendChild(style);
+            """)
+        )
