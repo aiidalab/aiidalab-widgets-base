2fa62c54f0fe6e69a781d905791bd2a77d08cabc
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2026-05-22 12:15:29 +0100

    tests: Simplify generate_calc_job_node fixture (#752)
    This fixture has clearly been copied over from
    aiida-quantumespresso plugin and even has some qe-specific
    and imports which we definitely don't need!

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,5 @@
 import io
 import json
-import os
-import shutil
 import time
 import uuid
 from collections.abc import Mapping
@@ -51,21 +49,16 @@ def generate_calc_job_node(aiida_localhost):
     def _generate_calc_job_node(
         entry_point_name="base",
         computer=None,
-        test_name=None,
         inputs=None,
         attributes=None,
-        retrieve_temporary=None,
     ):
         """Fixture to generate a mock `CalcJobNode` for testing parsers.
+
         :param entry_point_name: entry point name of the calculation class
         :param computer: a `Computer` instance
-        :param test_name: relative path of directory with test output files in the `fixtures/{entry_point_name}` folder.
         :param inputs: any optional nodes to add as input links to the corrent CalcJobNode
         :param attributes: any optional attributes to set on the node
-        :param retrieve_temporary: optional tuple of an absolute filepath of a temporary directory and a list of
-            filenames that should be written to this directory, which will serve as the `retrieved_temporary_folder`.
-            For now this only works with top-level files and does not support files nested in directories.
-        :return: `CalcJobNode` instance with an attached `FolderData` as the `retrieved` node.
+        :return: `CalcJobNode` instance
         """
         from aiida import orm
         from aiida.common import LinkType
@@ -74,16 +67,6 @@ def generate_calc_job_node(aiida_localhost):
 
         if computer is None:
             computer = aiida_localhost
-
-        filepath_folder = None
-
-        if test_name is not None:
-            basepath = os.path.dirname(os.path.abspath(__file__))
-            filename = os.path.join(
-                entry_point_name[len("quantumespresso.") :], test_name
-            )
-            filepath_folder = os.path.join(basepath, "parsers", "fixtures", filename)
-            filepath_input = os.path.join(filepath_folder, "aiida.in")
 
         entry_point = format_entry_point_string("aiida.calculations", entry_point_name)
 
@@ -97,22 +80,8 @@ def generate_calc_job_node(aiida_localhost):
         if attributes:
             node.base.attributes.set_many(attributes)
 
-        if filepath_folder:
-            from aiida_quantumespresso.tools.pwinputparser import PwInputFile
-            from qe_tools.exceptions import ParsingError
-
-            try:
-                with open(filepath_input, encoding="utf-8") as input_file:
-                    parsed_input = PwInputFile(input_file.read())
-            except (ParsingError, FileNotFoundError):
-                pass
-            else:
-                inputs["structure"] = parsed_input.get_structuredata()
-                inputs["parameters"] = orm.Dict(parsed_input.namelists)
-
         if inputs:
-            metadata = inputs.pop("metadata", {})
-            options = metadata.get("options", {})
+            options = inputs.pop("metadata", {}).get("options", {})
 
             for name, option in options.items():
                 node.set_option(name, option)
@@ -124,41 +93,6 @@ def generate_calc_job_node(aiida_localhost):
                 )
 
         node.store()
-
-        if retrieve_temporary:
-            dirpath, filenames = retrieve_temporary
-            for filename in filenames:
-                try:
-                    shutil.copy(
-                        os.path.join(filepath_folder, filename),
-                        os.path.join(dirpath, filename),
-                    )
-                except FileNotFoundError:
-                    pass  # To test the absence of files in the retrieve_temporary folder
-
-        if filepath_folder:
-            retrieved = orm.FolderData()
-            retrieved.base.repository.put_object_from_tree(filepath_folder)
-
-            # Remove files that are supposed to be only present in the retrieved temporary folder
-            if retrieve_temporary:
-                for filename in filenames:
-                    try:
-                        retrieved.base.repository.delete_object(filename)
-                    except OSError:
-                        pass  # To test the absence of files in the retrieve_temporary folder
-
-            retrieved.base.links.add_incoming(
-                node, link_type=LinkType.CREATE, link_label="retrieved"
-            )
-            retrieved.store()
-
-            remote_folder = orm.RemoteData(computer=computer, remote_path="/tmp")
-            remote_folder.base.links.add_incoming(
-                node, link_type=LinkType.CREATE, link_label="remote_folder"
-            )
-
-            remote_folder.store()
 
         # Set process state and exit status
         node.set_process_state(ProcessState.FINISHED)
